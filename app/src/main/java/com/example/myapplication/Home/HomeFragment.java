fc4500ca460b5937313807cd367aba4f318ed297
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-06-28 17:45:55 +0900

    액티비티 추가

--- app/src/main/java/com/example/myapplication/Home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/Home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.Home;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,12 +10,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+    FragmentHomeBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_home, container, false);
-        return v;
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        binding.imgvPlus.setOnClickListener(v -> {
+            startActivity(new Intent(this.getContext(), HomePlusActivity.class));
+        });
+        binding.svSearch.setOnClickListener(v -> {
+            startActivity(new Intent(this.getContext(), HomeSearchActivity.class));
+        });
+        binding.imgvMore.setOnClickListener(v -> {
+            startActivity(new Intent(this.getContext(), HomeMoreActivity.class));
+        });
+        binding.issues.setOnClickListener(v -> {
+            startActivity(new Intent(this.getContext(), HomeIssuesActivity.class));
+        });
+
+        return binding.getRoot();
     }
 }
