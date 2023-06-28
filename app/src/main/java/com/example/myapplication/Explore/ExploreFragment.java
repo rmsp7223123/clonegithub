851b4d2b755b55054c8ab8792456665c07bac26f
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-06-28 12:26:16 +0900

    메인액티비티에 익스플로러 프래그먼트 이동 추가 및 수정

---
+++ app/src/main/java/com/example/myapplication/Explore/ExploreFragment.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.Explore;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+public class ExploreFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_explore, container, false);
+        return v;
+    }
+}
