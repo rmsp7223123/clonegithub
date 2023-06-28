ec572345190702c223a901bf4da2274857f0e0f2
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-06-28 11:41:51 +0900

    메인액티비티 fragment 추가를 위한 수정

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
 
-        Window window =getWindow();
+        Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         window.setStatusBarColor(Color.TRANSPARENT);
         View view = getWindow().getDecorView();
         view.setSystemUiVisibility(view.getSystemUiVisibility() & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        // 네비바 클릭시 프래그먼트 전환
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            if (item.getItemId() == R.id.home) {
+//                fragment = new
+            } else if (item.getItemId() == R.id.notifications) {
+
+            } else if (item.getItemId() == R.id.notifications) {
+
+            } else if (item.getItemId() == R.id.notifications) {
+
+            } else {
+                return false;
+            }
+            if (fragment == null) {
+                Toast.makeText(this, "빈공간", Toast.LENGTH_SHORT).show();
+            } else {
+                manager.beginTransaction().replace(R.id.)
+            }
+        });
 
 
     }
