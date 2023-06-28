96c39acc5165b44978928f99cc9a5fe4b7a49305
615637 <129474590+615637@users.noreply.github.com>
2023-06-28 18:46:41 +0900

    Intent
    Fragment, Adapter에 Intent 연결

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
+import com.example.myapplication.Notifications.NotificationsFragment;
+import com.example.myapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    FragmentManager manager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        manager.beginTransaction().replace(R.id.container, new NotificationsFragment()).commit();
+
     }
 }
--- app/src/main/java/com/example/myapplication/Notifications/NotificationsAdapter.java
+++ app/src/main/java/com/example/myapplication/Notifications/NotificationsAdapter.java
@@ -1,4 +1,54 @@
 package com.example.myapplication.Notifications;
 
-public class NotificationsAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.MainActivity;
+import com.example.myapplication.databinding.ItemNotificationsBinding;
+
+public class NotificationsAdapter extends RecyclerView.Adapter<NotificationsAdapter.ViewHolder> {
+    ItemNotificationsBinding binding;
+    Context context;
+
+    public NotificationsAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        binding = ItemNotificationsBinding.inflate(inflater, parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder h, int i) {
+        h.binding.noLnItem.getContext();
+
+        h.binding.noLnItem.setOnClickListener(v -> {
+            Intent intent = new Intent(context, NotificationsDetailActivity.class);
+            context.startActivity(intent);
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ItemNotificationsBinding binding;
+
+        public ViewHolder(ItemNotificationsBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
 }
