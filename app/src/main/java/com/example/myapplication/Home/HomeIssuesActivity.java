4268aebc47e4d0a5ea0422bfeaa93eb864ae4425
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-06-29 09:17:04 +0900

    231

--- app/src/main/java/com/example/myapplication/Home/HomeIssuesActivity.java
+++ app/src/main/java/com/example/myapplication/Home/HomeIssuesActivity.java
@@ -2,15 +2,29 @@ package com.example.myapplication.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.ActivityHomeIssuesBinding;
 
 public class HomeIssuesActivity extends AppCompatActivity {
 
+    ActivityHomeIssuesBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_issues);
+        binding = ActivityHomeIssuesBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+        Window window =getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        window.setStatusBarColor(Color.TRANSPARENT);
+        View view = getWindow().getDecorView();
+        view.setSystemUiVisibility(view.getSystemUiVisibility() & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 }
